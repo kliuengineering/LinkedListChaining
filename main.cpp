fd18f6ab45d70a4eb093b57a4a5baa447d091bb9
kevin <kliuengineering@gmail.com>
2024-02-21 01:19:23 -0500

    code base recovery... need to fix the syntaxes for the codes to work...

--- main.cpp
+++ main.cpp
@@ -1,1 +1,71 @@
 // hello world...
+
+// following codes were lost and recovered.
+// syntaxes are incorrect due to the recovery.
+// however, the logics are perfectly fine.
+// may need to add brackets manually to make the following codes compile.
+// mind the indentation for adding the brackets.
+
+#include <iostream>
+#define TABLE_SIZE 100
+template <class T>
+class HashTable
+private:
+	typedef struct Node {
+		Node* next;
+		T data;
+		// constructor
+		Node(const T& data) : next(nullptr), data(data) {};
+	} Node;
+	int hash_function(const int& key)
+		return key % TABLE_SIZE;
+	// 2D table
+	Node* table[TABLE_SIZE];
+public:
+	// constructor
+	HashTable(void)
+		for (int i = 0; i < TABLE_SIZE; i++) table[i] = nullptr;
+	// destructor
+	~HashTable(void)
+		for (int i = 0; i < TABLE_SIZE; i++)
+			Node* current = table[i];
+	while (current != nullptr)
+		Node* previous = current;
+	current = current->next;
+	delete previous;
+	// insert
+	void insert(int key)
+		int hash_value = hash_function(key);
+	Node* prev = nullptr;
+	Node* current = table[hash_value];
+	// search for empty slot in a chain
+	while (current != nullptr)
+		prev = current;
+	current = current->next;
+	// allocate heap memory on the current ptr
+	if (current == nullptr)
+	{
+		current = new Node(key);
+		if (prev == nullptr)
+		{
+			table[hash_value] = current;
+		}
+		else prev->next = current;
+	}
+	//		else
+	//			current->data = key;
+	bool search(int key)
+		int hash_value = hash_function(key);
+	Node* current = table[hash_value];
+	while (current != nullptr)
+		if (current->data == key) return true;
+		else current = current->next;
+	return false;
+	void display(void)
+		for (int i = 0; i < TABLE_SIZE; i++)
+			Node* itr = table[i];
+	std::cout << "[ " << i << " ]";
+	while (itr != nullptr)
+		std::cout << " -> " << itr->data;
+	itr = itr->next;
+	puts("");
